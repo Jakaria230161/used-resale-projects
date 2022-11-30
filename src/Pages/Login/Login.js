174f0dac9aaed62e3dc0063853bef2aa791b5970
jakaria230161 <jakariamahmud016@gmail.com>
2022-11-30 12:49:08 +0600

    login added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -96,7 +96,7 @@ const Login = () => {
                     {
                         loginError && <p className='text-red-500'>{loginError}</p>
                     }
-                    <p>New to Doctors Portal <Link className='text-secondary' to="/signup">Create new Account</Link></p>
+                    <p>New to Mobixpress Ltd? <Link className='text-secondary' to="/signup">Create new Account</Link></p>
                     <div className="divider">OR</div>
                     <button onClick={handleGoogleLogin} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
                 </form>
