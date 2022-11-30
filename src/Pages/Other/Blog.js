fec389e90bcd948d732407020c83953a18a6ee82
jakaria230161 <jakariamahmud016@gmail.com>
2022-11-30 12:32:19 +0600

    footer added

--- src/Pages/Other/Blog.js
+++ src/Pages/Other/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
     return (
         <section className="dark:bg-gray-800 dark:text-gray-100">
             <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
-                <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">How it works</p>
+                <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">Welcome to my Blog</p>
                 <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
                 <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
                     <div>
--- src/Pages/Other/ErrorPage/ErrorPage.js
+++ src/Pages/Other/ErrorPage/ErrorPage.js
@@ -9,7 +9,7 @@ const ErrorPage = () => {
                 <div className="notfound-404">
                     <h1>4<span>0</span>4</h1>
                 </div>
-                <h2>the page you requested could not found</h2>
+                <h2>The page you requested could not found</h2>
                 <Link to='/' className="button px-3 py-2 rounded">GO to back</Link>
             </div>
         </div>
